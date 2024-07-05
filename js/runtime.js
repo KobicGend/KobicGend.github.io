156e0951a24c2311d102dd257e388bfee5178ccd
KobicGend <cmg77@126.com>
2024-07-05 14:14:12 +0800

    Site updated: 2024-07-05 14:13:43

---
+++ js/runtime.js
@@ -0,0 +1,1 @@
+$(function(){var o=Math.round(new Date("03/25/2024 16:20:38").getTime()/1e3);$("#runtime").flipcountdown({size:"xs",tick:function(){function n(n){return 9<n?n:"0"+n}var t=Math.abs(Math.round((new Date).getTime()/1e3)-o),e=86400,r=3600,a=parseInt(t/e),i=parseInt(t%e/r),u=parseInt(t%e%r/60);return n(a)+" "+n(i)+" "+n(u)+" "+n(t%e%r%60)}})});
