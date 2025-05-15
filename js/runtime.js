cb989041b10445c46a055c83712ddea1efd6b0f7
陈旻庚 <149890112+KobicGend@users.noreply.github.com>
2025-05-15 18:21:51 +0800

    Site updated: 2025-05-15 18:19:59

---
+++ js/runtime.js
@@ -0,0 +1,1 @@
+$((function(){var n=Math.round(new Date("03/25/2024 16:20:38").getTime()/1e3);$("#runtime").flipcountdown({size:"xs",tick:function(){var t=function(n){return n>9?n:"0"+n},e=Math.abs(Math.round((new Date).getTime()/1e3)-n),r=86400,a=3600,i=parseInt(e/r),u=parseInt(e%r/a),o=parseInt(e%r%a/60),s=e%r%a%60;return t(i)+" "+t(u)+" "+t(o)+" "+t(s)}})}));
