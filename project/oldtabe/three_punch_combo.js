8ce2defe4f5da1a2a9d468437b5fd00789fb4fb9
陈旻庚 <149890112+KobicGend@users.noreply.github.com>
2026-02-16 19:15:07 +0800

    Site updated: 2026-02-16 19:15:00

---
+++ project/oldtabe/three_punch_combo.js
@@ -0,0 +1,1 @@
+$i=0,$("#start").click(function(){$i++,$i>=6&&($("#start").hide(),$("#stop").show())}),$("#stop").click(function(){alert("这么作？今天别吃了！"),$(this).hide()}),$("#close_banner").click(function(){$("#banner").hide()});
